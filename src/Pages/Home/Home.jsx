ab1d5491f14413e921af75323363046e23c35cb5
zailua <84216764+zailua@users.noreply.github.com>
2022-03-18 08:05:09 -0300

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Type from '../../components/Writer/writer';
-import './Home.css';
+import Rain from '../../components/Rain/rain'
+import './Home.css'
 const Home = () => {
-  return (
-    <div className="bg">
-      <Type></Type>
-    </div>
-  )
+	return (
+		<div className="bg">
+			<Rain></Rain>
+			<Type></Type>
+		</div>
+	)
 }
 
 export default Home
---
+++ src/components/Rain/Rain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactRain from 'react-rain-animation'
+import 'react-rain-animation/lib/style.css'
+
+const Rain = props => {
+	return <ReactRain numDrops="25" />
+}
+
+export default Rain
