080a54c284b16bddbbb1c8665bd3356d239eb8f9
zailua <84216764+zailua@users.noreply.github.com>
2022-03-18 07:35:52 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
 		<header>
 			<nav>
 				<label className="logo">
-					<img src="./src/components/Header/sakura.png" alt="logo.png" />
+					<img src="./src/components/Header/sakura.png" alt="log.png" />
 				</label>
 				<input type="checkbox" id="check" />
 				<label for="check" className="button">
